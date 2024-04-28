abfc581087628478bf204469a2c5512659a9aa51
ff2400t <84376771+ff2400t@users.noreply.github.com>
2024-04-28 09:33:35 +0530

    adding icons userdirs and list view and some typing for the components

--- src/UserDirs.tsx
+++ src/UserDirs.tsx
@@ -1,21 +1,29 @@
-import { For } from "solid-js"
+import { Home, File, Download, Music, Image, Video } from "lucide-solid"
+import { Accessor, For, JSXElement, Show, createSelector } from "solid-js"
 
-export default function UserDirs(props) {
-  const userDirsList = [
-    "Home",
-    "Documents",
-    "Downloads",
-    "Music",
-    "Pictures",
-    "Videos",
+export default function UserDirs(props: { path: Accessor<string>; userDirs: Accessor<Record<string, string>>; setPath: (arg0: any) => void; }) {
+  const isSelected = createSelector(props.path);
+  const userDirsList: [string, JSXElement][] = [
+    ["Home", <Home />],
+    ["Documents", <File />],
+    ["Downloads", <Download />],
+    ["Music", <Music />],
+    ["Pictures", <Image />],
+    ["Videos", <Video />],
   ]
 
   return <section>
     <ul class="user-dirs">
       <For each={userDirsList} >
-        {(item) => <li onClick={() => props.setPath(props.userDirs()![item])}>
-          {item}
-        </li>}
+        {([name, icon]) => {
+          const path = props.userDirs()![name];
+          console.log(userDirsList, props.userDirs())
+          return <li classList={{ selected: isSelected(path) }} onClick={() => {
+            return props.setPath(path);
+          }}>
+            {icon} {name}
+          </li>;
+        }}
       </For>
     </ul>
   </section>
