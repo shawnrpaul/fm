8b02287cc7b782db1bc1207b5b23ca470a73768c
ff2400t <84376771+ff2400t@users.noreply.github.com>
2024-04-28 13:04:25 +0530

    added some icon for image, audio, vidoe  based on mime types

--- src/ListView.tsx
+++ src/ListView.tsx
@@ -1,15 +1,27 @@
 import { For, Resource, Show } from "solid-js";
 import { Entry } from "./types";
-import { Folder, File } from "lucide-solid";
+import { Folder, File, FileImage, FileAudio2, FileVideo } from "lucide-solid";
+import { invoke } from "@tauri-apps/api";
 
 export default function ListView(props: { items: Resource<Entry[]>; setPath: (arg0: string) => void; }) {
+
+  const getIcon = (item: Entry) => {
+    if (item.is_dir) return <Folder />
+    else if (item.mime_type.startsWith('image')) return <FileImage />
+    else if (item.mime_type.startsWith('audio')) return <FileAudio2 />
+    else if (item.mime_type.startsWith("video")) return <FileVideo />
+    return <File />
+  }
+
   return <section class='list-view'>
     <Show when={!props.items.loading}>
       <ul class='list-view-list'>
         <For each={props.items()} >
-          {(item) => <li onClick={() => item.is_dir && props.setPath(item.path)}>
-            {item.is_dir ? <Folder /> :
-              <File />}
+          {(item) => <li onClick={() => {
+            if (item.is_dir) { props.setPath(item.path); }
+            else invoke("open_file", { path: item.path })
+          }}>
+            {getIcon(item)}
             <span>{item.name}</span>
           </li>}
         </For>
