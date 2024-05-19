d1b2cd2d4bac8b5ac80bd685e080ca0851e5533c
Srpboyz <spaul030@gmail.com>
2024-05-19 11:39:42 -0700

    Error handled get_user_dirs if a path doesn't exist

--- src-tauri/src/filesystem/api.rs
+++ src-tauri/src/filesystem/api.rs
@@ -26,6 +26,7 @@ pub fn get_settings() -> Result<serde_json::Value, String> {
         };
         let settings: serde_json::Value =
             serde_json::from_str(&settings_data).expect("Unable to parse");
+
         return Ok(settings);
     };
     Err(String::from("Home directory couldn't be retrieved"))
@@ -44,7 +45,6 @@ pub fn update_settings(setting: String, value: serde_json::Value) -> Result<(), 
         settings[setting] = value;
 
         let settings_data = serde_json::to_string_pretty(&settings).unwrap();
-
         fs::write(&settings_path, settings_data.as_bytes()).unwrap();
 
         return Ok(());
@@ -101,9 +101,13 @@ pub fn get_user_dirs() -> Result<Vec<Entry>, String> {
         // add the remaining user dirs if exists
         for user_dir_path in user_dir_paths {
             if let Some(user_dir) = user_dir_path {
-                let name = user_dir.file_name().unwrap().to_str().unwrap().to_string();
-                let path = user_dir.canonicalize().unwrap();
-                directories.push(Entry::new(name, path, true, 0, false, String::new()));
+                match user_dir.canonicalize() {
+                    Ok(path) => {
+                        let name = user_dir.file_name().unwrap().to_str().unwrap().to_string();
+                        directories.push(Entry::new(name, path, true, 0, false, String::new()));
+                    }
+                    Err(_) => (),
+                }
             }
         }
         return Ok(directories);
@@ -130,7 +134,6 @@ pub fn get_dir_content(path: String) -> Result<Vec<Entry>, String> {
                 let is_dir = entry_info.is_dir();
                 let size = if is_dir { 0 } else { entry_info.len() };
                 let mime_type = utils::get_mime_type(&path);
-
                 let is_hidden = utils::is_entry_hidden(dir_entry);
 
                 entries.push(Entry::new(name, path, is_dir, size, is_hidden, mime_type));
